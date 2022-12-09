e4d19f18eb0b433afef33a028f306415cb95091e
KNOWLEDGEINFOTE\sathish.ch <sathish_ch>
2022-12-09 11:44:29 +0530

    setup

---
+++ src/maskingnumber/NumberMask.java
@@ -0,0 +1,26 @@
+package maskingnumber;
+
+public class NumberMask {
+	
+	public static String maskNum(Long number) {
+//		StringBuilder sb = new StringBuilder(number.toString());
+		StringBuffer sb = new StringBuffer(number.toString());
+		String x = "X".repeat(sb.length()-4);
+		return x+sb.substring(sb.length()-4, sb.length());
+	}
+	
+	public static String maskNum(String name) {
+		StringBuffer sb = new StringBuffer(name);
+		String x = "X".repeat(sb.length()-4);
+		return x+sb.substring(sb.length()-4, sb.length());
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println(maskNum(9490524580L));
+		System.out.println(maskNum("086610100079052"));
+		System.out.println(maskNum("SATHISH KUMAR"));
+		
+	}
+
+}
