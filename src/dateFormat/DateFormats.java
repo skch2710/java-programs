d396c7ae6941201ee4d889cdb80afa9f0033f7d4
sathish kumar <sathish.ch@excellarate.com>
2023-01-30 12:06:59 +0530

    date

---
+++ src/dateFormat/DateFormats.java
@@ -0,0 +1,44 @@
+package dateFormat;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class DateFormats {
+	
+	public static final String DATE_FORMAT= "MM/dd/yyyy";
+	
+	/**
+	 * String format.
+	 *
+	 * @param input the input
+	 * @return the string
+	 */
+	public static String stringFormat(String input) {
+		String output = "";
+		if (input.isEmpty()) {
+			output = String.format("%011d", Long.parseLong(input));
+		}
+		return output;
+	}
+	
+	public static void main(String[] args) throws ParseException {
+		System.out.println(new SimpleDateFormat(DATE_FORMAT).parse("10/27/1996"));
+		
+		System.out.println(new SimpleDateFormat( DATE_FORMAT ).format( new Date()));
+		
+//		System.out.println(DateFormats.stringFormat(123456));
+		
+		//TO CHECK DATE FORMAT
+        List<String> dates = new ArrayList<>();
+        dates.add("01/25/2023 or 1");
+        System.out.println(Utility.checkDateInjection(dates));
+        
+        //TO CHECK EMAIL VALIDATION
+        System.out.println(Utility.checkEmailInjection("SKCH2710@GMAIL.com or 1"));
+        
+	}
+
+}
---
+++ src/dateFormat/Utility.java
@@ -0,0 +1,44 @@
+package dateFormat;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Utility {
+
+	/**
+	 * method to check date validation.
+	 * 
+	 * @param dates
+	 * @return Boolean
+	 */
+	public static Boolean checkDateInjection(List<String> dates) {
+		Pattern p = Pattern.compile(
+				"^(?:(?:(?:0?[13578]|1[02])(\\/|-|\\.)31)\\1|(?:(?:0?[1,3-9]|1[0-2])(\\/|-|\\.)(?:29|30)\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:0?2(\\/|-|\\.)29\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:(?:0?[1-9])|(?:1[0-2]))(\\/|-|\\.)(?:0?[1-9]|1\\d|2[0-8])\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$");
+		Boolean result = true;
+		for (String date : dates) {
+			Matcher m = p.matcher(date);
+			if (date != null && !date.isEmpty() && !m.matches()) {
+				result = false;
+				break;
+			}
+		}
+		return result;
+	}
+
+	/**
+	 * method to check email validation.
+	 * 
+	 * @param mailId
+	 * @return Boolean
+	 */
+	public static Boolean checkEmailInjection(String mailId) {
+		String regex = "^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(mailId);
+		if (mailId != null && !mailId.isEmpty() && !m.matches()) {
+			return false;
+		}
+		return true;
+	}
+}
