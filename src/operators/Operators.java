2611b5ccce03de9f49cb854cf732a9ff2ae94f43
sathish kumar <sathish.ch@excellarate.com>
2023-02-17 13:04:20 +0530

    ss

--- src/operators/Operators.java
+++ src/operators/Operators.java
@@ -4,26 +4,42 @@ package operators;
  * 
  * @author sathish
  *
- *	1.Arithmetic Operators ( +,-,*,/,% )
- *	2.Unary Operators 
- *		i.unary minus(-)
- *		ii.Increment(++) [ pre & post ]
- *		iii.Decrement(--) [ pre & post ]
- *	3.Assignment Operators ( = , +=,-= )
- *	4.Relational Operators ( == , < , <= , >= ,!= )
- *	5.Logical Operators ( && - and , || - or, ! - not)
- *		i. && - check 
- *
- *			 the "&&" operator is used as a logical AND
- *         operator. It returns true if both operands are true, and false
- *         otherwise. The expression using "&&" is only evaluated further if the
- *         first operand evaluates to true, otherwise it stops and returns false
- *         immediately.
+ *         1.Arithmetic Operators ( +,-,*,/,% ) 
+ *         2.Unary Operators 
+ *         		i.unary minus(-) 
+ *         		ii.Increment(++) [ pre & post ] 
+ *         		iii.Decrement(--) [ pre & post ] 
+ *         3.Assignment Operators ( = , +=,-= ) 
+ *         4.Relational Operators (== , < , <= , >= ,!= ) 
+ *         5.Logical Operators ( && - and , || - or, ! -not) 
  *         
- *      ii . || -
- *      
- *       the "||" operator is used as a logical OR operator. It returns true if either one of the operands is true, and false otherwise. The expression using "||" is only evaluated further if the first operand evaluates to false, otherwise it stops and returns true immediately.
+ *         	  i. && - check only one condition
+ *         		 & - check double condition
+ *
+ *         			the "&&" operator is used as a logical AND operator. It returns true
+ *         		if both operands are true, and false otherwise. The expression using
+ *         		"&&" is only evaluated further if the first operand evaluates to
+ *         		true, otherwise it stops and returns false immediately.
+ * 
+ *         	   ii . || 
+ * 
+ *         			the "||" operator is used as a logical OR operator. It returns true
+ *         		if either one of the operands is true, and false otherwise. The
+ *        		expression using "||" is only evaluated further if the first operand
+ *        		evaluates to false, otherwise it stops and returns true immediately.
+ *        
+ *        		==>> AND -- Both true then return true
+ *        		==>> OR -- Any one true then return true
+ *        
+ *        6. Ternary Operator
+ *        
+ *        		condition ? true : false ;
  */
 public class Operators {
-
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
 }
