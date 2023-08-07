bc7b575b8caf770ac5f0176f4beaec5c35987370
sathish_ch <sathish.ch@excellarate.com>
2023-08-07 21:12:06 +0530

    streams

--- src/java8Features/Streams.java
+++ src/java8Features/Streams.java
@@ -5,6 +5,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Streams {
+	
+	public static String listToString(List<?> list) {
+		return list.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(","));
+	}
 
 	/**
 	 * List of Objects to List of Long
@@ -23,6 +29,8 @@ public class Streams {
 		List<Long> longs = myObjects.stream().map(MyObject::getId).collect(Collectors.toList());
 
 		System.out.println(longs);
+		
+		System.out.println("List of Long to String :: "+ listToString(longs) );
 	}
 
 }
