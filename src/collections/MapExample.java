2e49751b354bd55077efde6293ed74dba15fa2ba
sarhish_ch <skch210@outlook.com>
2023-09-09 15:15:02 +0530

    map

---
+++ src/collections/MapExample.java
@@ -0,0 +1,17 @@
+package collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapExample {
+	
+	public static void main(String[] args) {
+		Map<String,Object> map = new HashMap<>();
+		map.put("type", "application/zip");
+		map.put("fileName", "ex.zip");
+		
+		System.out.println(map.get("type").toString());
+		System.out.println(map.get("fileName").toString());
+	}
+
+}
