71590f1cc3dcb29d3eb6493a3549094d0b0d93ac
sarhish_ch <skch210@outlook.com>
2023-10-10 16:33:54 +0530

    streems ex

--- src/java8Features/MyObject.java
+++ src/java8Features/MyObject.java
@@ -1,15 +1,14 @@
 package java8Features;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MyObject {
 
 	private Long id;
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
 
 }
--- src/java8Features/Streams.java
+++ src/java8Features/Streams.java
@@ -17,20 +17,48 @@ public class Streams {
 	 */
 	public static void main(String[] args) {
 
-		MyObject myObject = new MyObject();
-		myObject.setId(8L);
-		MyObject myObject1 = new MyObject();
-		myObject1.setId(4L);
+		MyObject myObject = new MyObject(2L);
+		MyObject myObject1 = new MyObject(2L);
+		MyObject myObject2 = new MyObject(2L);
+		MyObject myObject3 = new MyObject(2L);
+		MyObject myObject4 = new MyObject(2L);
+		MyObject myObject5 = new MyObject(2L);
 
 		List<MyObject> myObjects = new ArrayList<>();
 		myObjects.add(myObject);
 		myObjects.add(myObject1);
+		myObjects.add(myObject2);
+		myObjects.add(myObject3);
+		myObjects.add(myObject4);
+		myObjects.add(myObject5);
 
 		List<Long> longs = myObjects.stream().map(MyObject::getId).collect(Collectors.toList());
 
 		System.out.println(longs);
 		
 		System.out.println("List of Long to String :: "+ listToString(longs) );
+		
+		//allMatch 
+		Boolean cond = myObjects.stream().allMatch(obj -> obj.getId() == 2L);
+		
+		System.out.println(cond);
+		
+		/**
+		 * anyMatch will return true if there is at least one element 
+		 * in the myObjects list for which obj.getId() equals 2L, 
+		 * and it will return false if none of the elements match the condition.
+		 */
+		Boolean cond1 = myObjects.stream().anyMatch(obj -> obj.getId() == 2L);
+		
+		System.out.println(cond1);
+		
+		Long count = myObjects.stream().filter(obj -> obj.getId() == 2L).count();
+
+		System.out.println(count);
+		
+		Double percentage = (double) count / myObjects.size() * 100.0;
+
+		System.out.println("Percentage: " + Math.round(percentage) + "%");
 	}
 
 }
