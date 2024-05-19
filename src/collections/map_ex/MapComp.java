8d7381422ac0196fed67da79a8b1964659a7e03c
sarhish_ch <skch210@outlook.com>
2024-05-19 22:05:50 +0530

    with map

--- src/collections/map_ex/MapComp.java
+++ src/collections/map_ex/MapComp.java
@@ -3,7 +3,9 @@ package collections.map_ex;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class MapComp {
 
@@ -12,6 +14,9 @@ public class MapComp {
 		List<Model> dbList = new ArrayList<>();
 		dbList = Arrays.asList(new Model(1L, "Y"), new Model(2L, "Y"), new Model(3L, "Y"),
 				new Model(4L, "Y"), new Model(5L, "Y"));
+
+		Map<Long, Model> mapList = dbList.stream()
+				.collect(Collectors.toMap(Model::getId, model -> model));
 		
 		List<Model> listDTO = Arrays.asList(new Model(1L, "Y"), new Model(2L, "Y"), new Model(3L, "Y"),
 				new Model(4L, "Y"), new Model(5L, "Y"));
@@ -21,11 +26,14 @@ public class MapComp {
 			dbList.addAll(listDTO);
 		}else {
 			for(Model dto : listDTO) {
-				Optional<Model> model = dbList.stream()
-						.filter(obj -> obj.getId().equals(dto.getId()))
-						.findFirst();
-				if(model.isPresent()) {
-					model.get().setValue("P");
+//				Optional<Model> model = dbList.stream()
+//						.filter(obj -> obj.getId().equals(dto.getId()))
+//						.findFirst();
+//				if(model.isPresent()) {
+//				model.get().setValue("P");
+				if(mapList.containsKey(dto.getId())) {
+					Model exist = mapList.get(dto.getId());
+					exist.setValue("P");
 				}else {
 					dbList.add(new Model(0L,"P"));
 				}
